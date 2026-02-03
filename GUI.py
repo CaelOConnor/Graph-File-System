99c538a91e5bb0e693d22971b92006581e284144
CaelOConnor <coconnor10@students.stonehill.edu>
2026-02-03 18:54:43 -0500

    started PyQt from documentation

---
+++ GUI.py
@@ -0,0 +1,24 @@
+import sys
+
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Graph File System")
+
+        button = QPushButton("Press Me!")
+
+        self.setMinimumSize(QSize(800, 600))
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
