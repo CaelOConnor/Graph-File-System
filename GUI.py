78fdc5a75c48c46cbf70747468fe925765f97df3
CaelOConnor <coconnor10@students.stonehill.edu>
2026-02-19 18:12:13 -0500

    tried centering a button

--- GUI.py
+++ GUI.py
@@ -1,8 +1,10 @@
 import sys
 
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
+
+# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -11,10 +13,19 @@ class MainWindow(QMainWindow):
 
         button = QPushButton("Press Me!")
 
-        self.setMinimumSize(QSize(800, 600))
+        screen = QApplication.primaryScreen()
+        screen_size = screen.availableGeometry()
+
+        self.setMinimumSize(QSize(400, 300))
+        self.setMaximumSize(QSize(screen_size.width(), screen_size.height()))
+
+        button.setMinimumSize(50, 50)
+        button.setMaximumSize(300, 300)
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
+
+
 
 app = QApplication(sys.argv)
 
